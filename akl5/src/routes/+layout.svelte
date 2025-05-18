8bd81cc1fed324d5c3fe18ef8dbc637538fb3d60
Alice Lee <86637906+akl5@users.noreply.github.com>
2025-05-18 11:24:40 -0400

    initial build

---
+++ akl5/src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+
+<script lang="ts">    
+    // import stylesheet: 
+    import '../app.scss';
+
+    let darkMode = $state(false);
+
+    // icons for day/night mode toggle:
+    import { Icon } from 'svelte-icons-pack';
+    import { BsMoon,BsMoonStarsFill } from "svelte-icons-pack/bs";    
+    
+
+    let {children} = $props();
+
+    // toggling light/dark-mode 
+    function toggleTheme() { 
+        darkMode = !darkMode;
+    }
+
+</script> 
+
+<div class="menu-bar"> 
+    <div class="menu-selections"> 
+        <nav> 
+            <a href="/">home</a> 
+            <a href="/">about</a> 
+            <a href="/projects">projects</a> 
+            <a href="/">contact</a> 
+        </nav> 
+    </div>
+    <button class="toggle-theme-button" onclick={toggleTheme}>
+        {#if darkMode} 
+            <Icon src={BsMoonStarsFill}/>
+        {:else}
+            <Icon src={BsMoon}/>
+        {/if}
+    </button>
+    <p>darkMode: {darkMode ? 'true' : 'false'}</p>
+
+</div>
+
+{@render children()}
---
+++ akl5/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script> 
+    let elapsed = $state(0);
+    let interval = $state(1000);
+
+    $effect(() => {
+        setInterval(() => {
+            elapsed+=1;
+        }, interval);
+    });
+</script>
+
+<svelte:head> 
+    <title>alice lee.</title> 
+</svelte:head> 
+
+<h1>alice lee</h1>
+<h3>Boston, MA </h3>
+<p> time elapsed on page: {elapsed}s </p> 
